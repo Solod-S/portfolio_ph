d5baa51c16e715bc58dd713b2aa412b83d91b6c1
Sergey <104456968+Solod-S@users.noreply.github.com>
2023-09-24 08:28:21 +0300

    add gallery, add contact update, increase in productivity (remove cursor animation)
    - add gallery
    - add contact update
    - increase in productivity (remove cursor animation)

--- js/index.js
+++ js/index.js
@@ -5,16 +5,15 @@
 
   //Page cursors
 
-  document
-    .getElementsByTagName("body")[0]
-    .addEventListener("mousemove", function (n) {
-      (t.style.left = n.clientX + "px"),
-        (t.style.top = n.clientY + "px"),
-        (e.style.left = n.clientX + "px"),
-        (e.style.top = n.clientY + "px"),
-        (i.style.left = n.clientX + "px"),
-        (i.style.top = n.clientY + "px");
-    });
+  document.getElementsByTagName("body")[0];
+  // .addEventListener("mousemove", function (n) {
+  //   (t.style.left = n.clientX + "px"),
+  //     (t.style.top = n.clientY + "px"),
+  //     (e.style.left = n.clientX + "px"),
+  //     (e.style.top = n.clientY + "px"),
+  //     (i.style.left = n.clientX + "px"),
+  //     (i.style.top = n.clientY + "px");
+  // });
   var t = document.getElementById("cursor"),
     e = document.getElementById("cursor2"),
     i = document.getElementById("cursor3");
